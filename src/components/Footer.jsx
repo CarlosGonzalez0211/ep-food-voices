cbde5365d75c1d42e0847ceea7f82f23824952ec
Carlos Gonzalez <116935841+CarlosGonzalez0211@users.noreply.github.com>
2025-08-06 13:23:05 -0600

    footer implementation

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+// components/Footer.jsx
+import React from 'react';
+import styles from '../components/styles.module.css';
+
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <div className={styles.footerLeft}>
+            <p >
+                Contact <br/>
+                Meredith E. Abarca. Ph.D <br/>
+                Founder, Editor, Curator of EPFV <br/>
+                Professor of Food Studies and Literature <br/>
+                Department of English <br/>
+                The Univeristy of Texas at El Paso <br/>
+                <a href="mailto:mabarca@utep.edu">mabarca@utep.edu</a> 
+            </p>
+        </div>
+        <div className={styles.footerRight}>
+            <img src="/footer_images/utep_logo_white.png" alt="UTEP Logo" className={styles.footerLogo}/>
+            <img src="/footer_images/podbeans-logo.png" alt="Podbeans Logo" className={styles.footerLogo}/>
+            <img src="/footer_images/human_logo.png" alt="The Humanities Collaborative Logo" className={styles.footerLogo}/>
+            <img src="/footer_images/history_logo.png" alt="Institute of Oral History Logo" className={styles.footerLogo}/>
+        </div>
+        
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,19 @@ import styles from '../components/styles.module.css';
 const Header = () => {
   return (
     <header className={styles.navbar}>
-      <img src="/logo.png" alt="Logo" />
+      <img
+        src="/header_logo_epfv.png"
+        alt="Site Logo"
+        className={styles.logo}
+        />
       <nav className={styles.links}>
         <a href="#">Home</a>
+        <a href="#">Stories</a>
+        <a href="#">Recipes</a>
+        <a href="#">Podcasts</a>
+        <a href="#">Resources</a>
         <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Portfolio</a>
-        <a href="#">Blog</a>
-        <a href="#">Contact</a>
-        <a href="#">FAQ</a>
+        <a href="#">Epicureans</a>
       </nav>
     </header>
   );
